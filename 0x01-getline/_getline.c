2b2fb30b540b4bdb6d72d8f6412f5ae9982f5eb4
Athesto <gamez.live@gmail.com>
2021-04-26 16:33:05 -0500

    set static variables

--- 0x01-getline/_getline.c
+++ 0x01-getline/_getline.c
@@ -1,4 +1,5 @@
 #include "_getline.h"
+
 /**
  * _getline - custom getline
  * @fd: file descriptor to read from
@@ -6,20 +7,30 @@
  */
 char *_getline(const int fd)
 {
-	char local_buffer[READ_SIZE] = {0};
-	char *buffer;
-	ssize_t bytes;
+	static char local_buffer[READ_SIZE] = {0};
+	static char *tmp;
+	char *output;
+	int len;
 
-	bytes = read(fd, local_buffer, READ_SIZE);
-	if (bytes > 0)
+	if (local_buffer[0] == '\0')
 	{
-		buffer = malloc((bytes + 1) * sizeof(*buffer));
-		if (!buffer)
-			return (NULL);
-		strcpy(buffer, local_buffer);
-
-		return (buffer);
+		read(fd, local_buffer, READ_SIZE);
+		tmp = local_buffer;
 	}
 
-	return (NULL);
+	len = 0;
+	while (tmp[len] && tmp[len] != '\n')
+		len++;
+
+	if (!tmp[len])
+		return (NULL);
+
+	output = malloc((len + 1) * sizeof(*output));
+	if (!output)
+		return (NULL);
+
+	strncpy(output, tmp, len);
+	output[len] = '\0';
+	tmp = tmp + (len + 1);
+	return (output);
 }
