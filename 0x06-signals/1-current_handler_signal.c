754c72d43fb8720d81c69ddba71224bd6c9f62ff
Athesto <gamez.live@gmail.com>
2021-06-26 22:03:57 -0500

    1-current_handler_signal

---
+++ 0x06-signals/1-current_handler_signal.c
@@ -0,0 +1,15 @@
+#include "signals.h"
+/**
+ * current_handler_signal - retrieves the current handler of the signal SIGINT
+ * Return: a pointer to the current handler of SIGINT
+ */
+void (*current_handler_signal(void))(int)
+{
+	void (*handler)(int);
+	handler = signal(SIGINT, NULL);
+	signal(SIGINT, handler);
+
+	if (handler == SIG_ERR)
+		return (NULL);
+	return (handler);
+}
