11068eb72544d7cf5b6cefa92365e3119e75418b
Athesto <gamez.live@gmail.com>
2021-06-27 17:42:17 -0500

    2-handle_sigaction

--- 0x06-signals/2-handle_sigaction.c
+++ 0x06-signals/2-handle_sigaction.c
@@ -1,8 +1,23 @@
+#include "signals.h"
+
+/**
+ * printGotcha - print Gotcha and code
+ * @code: input code
+ */
+void printGotcha(int code)
+{
+	printf("Gotcha! [%d]\n", code);
+	fflush(stdout);
+}
+
 /**
  * handle_sigaction - set a handler for the signal SIGINT
  * Return: 0 on success, or -1 on error
  */
 int handle_sigaction(void)
 {
-	return (0);
+	sigaction_t act = {0}; /* vagrant is happy with a variable initalized */
+
+	act.sa_handler = printGotcha;
+	return (sigaction(SIGINT, &act, NULL));
 }
