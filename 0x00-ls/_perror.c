f553f23c4feee7c47301b64ab75c625eac6df74d
Athesto <gamez.live@gmail.com>
2021-07-16 13:53:25 -0500

    errors with checker

--- 0x00-ls/_perror.c
+++ 0x00-ls/_perror.c
@@ -7,10 +7,20 @@
 void _perror(char *argv)
 {
 	char msg[1024];
+	char *tmp;
 	char *program = globals()->program;
 	int *status = &globals()->status;
 
-	sprintf(msg, "%s: cannot access %s", program, argv);
+	switch (errno)
+	{
+		case EACCES:
+			tmp = "%s: cannot open directoy %s";
+			break;
+		default:
+			tmp = "%s: cannot access %s";
+
+	}
+	sprintf(msg, tmp, program, argv);
 	perror(msg);
 	*status = 2;
 }
