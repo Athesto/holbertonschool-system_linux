4d26c4728c0b61240036cbb69701c84c38326bb3
Athesto <gamez.live@gmail.com>
2021-07-17 00:12:03 -0500

    _perror with malloc

--- 0x00-ls/_perror.c
+++ 0x00-ls/_perror.c
@@ -6,7 +6,7 @@
  */
 void _perror(char *argv)
 {
-	char msg[1024];
+	char *msg;
 	char *tmp;
 	char *program = globals()->program;
 	int *status = &globals()->status;
@@ -20,7 +20,9 @@ void _perror(char *argv)
 			tmp = "%s: cannot access %s";
 
 	}
+	msg = malloc(strlen(tmp) + strlen(program) + strlen(argv));
 	sprintf(msg, tmp, program, argv);
 	perror(msg);
+	free(msg);
 	*status = 2;
 }
