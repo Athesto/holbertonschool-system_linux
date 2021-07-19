fba9e6f4075b0b502c7cb1202a353fb9928ef6c0
Athesto <gamez.live@gmail.com>
2021-07-19 09:49:26 -0500

    only prints

--- 0x00-ls/hls.c
+++ 0x00-ls/hls.c
@@ -7,16 +7,17 @@
 int hls(char *argv[])
 {
 	int *status = &globals()->status;
-	linked_t *file_list = NULL;
-	linked_t *dir_list = NULL;
+	/* linked_t *file_list = NULL; */
+	/* linked_t *dir_list = NULL; */
 
 
 	fprintf(stdout, "testing fprintf in stdout %d\n", 10);
-	splitargv(&dir_list, &file_list, argv);
-	print_content(dir_list, file_list);
+	/* splitargv(&dir_list, &file_list, argv); */
+	/* print_content(dir_list, file_list); */
 
 
-	list_free(dir_list);
+	/* list_free(dir_list); */
+	(void) argv;
 	fprintf(stdout, "testing fprintf in stdout at endu%d\n", 10);
 	return (*status);
 
