93408b472f1eb1f1b10b877e5ae4696238eaf237
Athesto <gamez.live@gmail.com>
2021-07-19 08:57:09 -0500

    _strlen if char exist

--- 0x00-ls/getpadding.c
+++ 0x00-ls/getpadding.c
@@ -8,19 +8,25 @@ padding_t getpadding(linked_t *list)
 {
 	linked_t *runner;
 	padding_t padding = {0}, max_pd = {0};
-	int len;
+	int len = 0;
 	struct stat mystat = {0};
 	int counter, tmp;
+	char *name, *group;
 
 	for (runner = list; runner; runner = runner->next)
 	{
 		getlstat(&mystat, runner);
 		max_pd.pad_links = max_t(max_pd.pad_links, (int)mystat.st_nlink);
 
-		len = _strlen(getpwuid(mystat.st_uid)->pw_name);
+		name = getpwuid(mystat.st_uid)->pw_name;
+		if (name)
+			len = _strlen(name);
 		max_pd.pad_usr = max_t(max_pd.pad_usr, len);
 
-		len = _strlen(getgrgid(mystat.st_uid)->gr_name);
+
+		group = getgrgid(mystat.st_uid)->gr_name;
+		if (group)
+			len = _strlen(group);
 		max_pd.pad_grp = max_t(max_pd.pad_grp, len);
 
 		max_pd.pad_size = max_t(max_pd.pad_size, mystat.st_size);
