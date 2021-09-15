2d13441da40dcbef4b13d530044525aa83f65bb1
Athesto <gamez.live@gmail.com>
2021-09-14 21:39:45 -0500

    0-strlen.asm

--- 0x05-libasm/0-strlen.asm
+++ 0x05-libasm/0-strlen.asm
@@ -1,5 +1,14 @@
         global asm_strlen
 
 asm_strlen:
-        mov rax, 1
+        push RBP
+        mov EAX, -1
+        mov ecx, 0
+loop:
+        inc EAX
+        mov CL, byte[RDI + RAX]
+        jecxz finish
+        jmp loop
+finish:
+        pop RBP
         ret
