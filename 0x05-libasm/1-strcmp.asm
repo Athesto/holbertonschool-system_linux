3205ce632b9531f1808267fa40cf32bc8200cb73
Athesto <gamez.live@gmail.com>
2021-09-16 20:49:45 -0500

    1-strcmp.asm

--- 0x05-libasm/1-strcmp.asm
+++ 0x05-libasm/1-strcmp.asm
@@ -1,4 +1,35 @@
         global asm_strcmp
 
 asm_strcmp:
+        push RBP
+        mov RAX, 0                ; char of string1
+        mov RBX, 0                ; char of string2
+        mov RCX, 0                ; diff of a and b
+        mov RDX, 0                ; counter of stings
+
+loop:
+        mov AL, [RDI + RDX]
+        mov BL, [RSI + RDX]
+        inc RDX
+        cmp AL, BL
+        jb ret_minus1
+        ja ret_plus1
+        cmp AL, 0
+        jnz ret_zero
+        jmp loop
+
+ret_plus1:
+        mov EAX, 1
+        jmp finish
+
+ret_zero:
+        mov EAX, 0
+        jmp finish
+
+ret_minus1:
+        mov EAX, -1
+        jmp finish
+
+finish:
+        pop RBP
         ret
