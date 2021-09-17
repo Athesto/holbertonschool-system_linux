91366b869bf0d4e573a59b4ef83d576ebcf6befb
Athesto <gamez.live@gmail.com>
2021-09-16 22:41:50 -0500

    3-strchr

--- 0x05-libasm/3-strchr.asm
+++ 0x05-libasm/3-strchr.asm
@@ -1,3 +1,25 @@
         global asm_strchr
 asm_strchr:
+        push RBP
+        mov RAX, 0                ; output string
+        mov RCX, 0                ; searching char
+        mov R8, 0                 ; dereference input string
+
+        mov RAX, RDI              ; initialize
+        mov CL, SIL
+
+loop:
+        cmp byte [RAX], CL
+        je finish
+        cmp byte [RAX], 0
+        je ret_zero
+        inc RAX
+        jmp loop
+
+ret_zero:
+        mov RAX, 0
+        jmp finish
+
+finish:
+        pop RBP
         ret
