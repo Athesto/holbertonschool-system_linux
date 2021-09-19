3d1140c4701c1cb0ad24dec13b88cbc424a47760
Athesto <gamez.live@gmail.com>
2021-09-18 20:08:01 -0500

    6-putc.asm

--- 0x05-libasm/6-putc.asm
+++ 0x05-libasm/6-putc.asm
@@ -1,3 +1,17 @@
         global asm_putc
+
 asm_putc:
+        push RBP                  ; save Base pointer
+        mov RBP, RSP              ; save Stack pointer
+
+        mov byte [RSP], DIL       ; load input
+write:
+        mov RAX, 1                ; syscall of write
+        mov RDI, 1                ; stdout
+        mov RSI, RSP              ; element
+        mov RDX, 1                ; count
+        syscall
+
+        mov RSP, RBP              ; restore previous Stack pointer
+        pop RBP                   ; restore previous Base pointer
         ret
